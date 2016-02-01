e870e45377bac4030ac08d7dc86309a8a0a83486
ChengLiu <ChengLiu@Freezing.local>
2016-02-01 18:04:09 +0800

    fix: path.

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (content, file, conf) {
+  console.log(file.realpath);
+
   conf.filename = file.realpath;
   var template = ejs.compile(content, conf);
 
-  var pathObject = path.parse(file.realpath);
-  pathObject.base = file.data;
-  
-  var dataPath = path.format(pathObject);
+  var dataPath = path.join(path.dirname(file.realpath), conf.data);
   var dataFile = fs.readFileSync(dataPath);
   var data = JSON.parse(dataFile);
 
