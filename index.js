9aaa5d0ea497e9f8c31caaaccf5de4914eec6baa
ChengLiu <ChengLiu@Freezing.local>
2016-02-22 13:01:15 +0800

    add configuration params

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var path = require('path');
 var nunjucks = require('nunjucks');
 
 module.exports = function (content, file, conf) {
-  var dataPath = path.join(path.dirname(file.realpath), conf.data);
+  var dataDir = path.dirname(file.realpath);
+  var dataPath = path.join(dataDir, conf.data);
   var dataFile = fs.readFileSync(dataPath);
   var data = JSON.parse(dataFile);
 
-  return nunjucks.renderString(content, data);
+  return nunjucks.configure(dataDir).renderString(content, data);
 };
