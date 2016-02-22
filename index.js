331bc7a0b65c5b3171615a0f858c23b0a356e727
ChengLiu <ChengLiu@Freezing.local>
2016-02-22 11:52:55 +0800

    change template engine

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 'use strict';
 
-var ejs = require('ejs');
 var fs = require('fs');
 var path = require('path');
+var nunjucks = require('nunjucks');
 
 module.exports = function (content, file, conf) {
-  console.log(file.realpath);
-
-  conf.filename = file.realpath;
-  var template = ejs.compile(content, conf);
-
   var dataPath = path.join(path.dirname(file.realpath), conf.data);
   var dataFile = fs.readFileSync(dataPath);
   var data = JSON.parse(dataFile);
 
-  return template(data);
+  return nunjucks.renderString(content, data);
 };
